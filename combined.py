1b7f9cb5f76bd2272a19c3c67bc20c3c3021c8a1
kattapistriphosphate <92348559+kattapis64@users.noreply.github.com>
2025-06-11 20:28:37 +0700

    Update combined.py

--- combined.py
+++ combined.py
@@ -34,7 +34,7 @@ username = os.getenv('US')
 pwd = os.getenv('pwd')
 url = "https://sso.satitm.chula.ac.th/adfs/oauth2/authorize?response_type=code&client_id=9d7865f9-7fe8-490f-bb72-25defaf77212&redirect_uri=https%3A%2F%2Fwww.mycourseville.com%2Fapi%2Fsatitm%2Fcallback"
 
-driver = webdriver.Chrome(service=Service("/usr/bin/chromedriver",options=options))
+driver = webdriver.Chrome(service=Service("/usr/bin/chromedriver"))
 driver.get(url)
 
 
