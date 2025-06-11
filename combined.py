b733145a51b85499238e189f6d350f1dc43b8aad
kattapistriphosphate <92348559+kattapis64@users.noreply.github.com>
2025-06-11 21:23:27 +0700

    Update combined.py

--- combined.py
+++ combined.py
@@ -8,6 +8,7 @@ from selenium.webdriver.common.by import By
 from sys import *
 import os
 from dotenv import load_dotenv
+import tempfile
 
 from email.message import EmailMessage
 import ssl
@@ -30,6 +31,8 @@ def split_list(lst, delimiter):
     return result
 options = Options()
 #options.add_argument("--headless")
+temp_dir = tempfile.mkdtemp()
+options.add_argument(f"--user-data-dir={temp_dir}")
 username = os.getenv('US')
 pwd = os.getenv('pwd')
 url = "https://sso.satitm.chula.ac.th/adfs/oauth2/authorize?response_type=code&client_id=9d7865f9-7fe8-490f-bb72-25defaf77212&redirect_uri=https%3A%2F%2Fwww.mycourseville.com%2Fapi%2Fsatitm%2Fcallback"
