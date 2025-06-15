73b57f0bc790bbf45df1bc179c599cdb9c9b1a80
kattapistriphosphate <92348559+kattapis64@users.noreply.github.com>
2025-06-15 21:32:47 +0700

    Update reader.py

--- reader.py
+++ reader.py
@@ -30,15 +30,25 @@ with open('log.txt') as f:
     for r in g:
         a.append(r.split(",")[0:-1])
 for k in range(len(a[0])):
-    a[0][k]=a[0][k][0:-13]
+    a[0][k]=a[0][k][0:-13].strip()
 for s in range(len(a[1])):
-    a[1][s]=a[1][s][0:-13]
+    a[1][s]=a[1][s][0:-13].strip()
 print(a[1])
+print(a[0])
 for i in a[0]:
     for j in a[1]:
+        print(i)
+        print(j)
+        print("")
+
         if i==j:
+            print("yay")
             first = a[0].index(i)
             later = a[1].index(i)
+            dummy=1
+            break
+    if dummy==1:
+        break
 print(first,later)
 print(a[1][0:later])
 
